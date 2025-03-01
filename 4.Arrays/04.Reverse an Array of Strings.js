228742f5b74a30a41e3b60f770c9eff78bcf7d26
DimitarBelchev <heavyartillery98@gmail.com>
2025-03-01 19:50:43 +0200

    Create 04.Reverse an Array of Strings.js

---
+++ 4.Arrays/04.Reverse an Array of Strings.js
@@ -0,0 +1,8 @@
+function reverseArrayOfStrings(array) {
+  const reversedArray = array.reverse();
+  console.log(reversedArray.join(" "));
+}
+
+reverseArrayOfStrings(["a", "b", "c", "d", "e"]);
+reverseArrayOfStrings(["abc", "def", "hig", "klm", "nop"]);
+reverseArrayOfStrings(["33", "123", "0", "dd"]);

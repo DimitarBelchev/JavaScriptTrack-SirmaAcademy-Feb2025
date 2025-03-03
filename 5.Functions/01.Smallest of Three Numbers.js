f0772af72a2767689ab2721c39897b65e92344bc
DimitarBelchev <heavyartillery98@gmail.com>
2025-03-03 18:53:49 +0200

    Create 01.Smallest of Three Numbers.js

---
+++ 5.Functions/01.Smallest of Three Numbers.js
@@ -0,0 +1,7 @@
+function findSmallestOfThree(a, b, c) {
+  return Math.min(a, b, c);
+}
+
+console.log(findSmallestOfThree(2, 5, 3));
+console.log(findSmallestOfThree(600, 342, 123));
+console.log(findSmallestOfThree(25, 21, 4));
